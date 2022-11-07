b2d1f7458770828c750fa3dd2ff539e29bc1960c
Jacob Bare <jacob@parameter1.com>
2022-11-07 12:31:15 -0600

    Export `Document` typedef

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,3 +1,7 @@
 export { ClientSession, Collection, MongoClient } from 'mongodb';
 export { filterMongoURL } from './filter-url.js';
 export { iterateMongoCursor } from './iterate-cursor.js';
+
+/**
+ * @typedef {import("mongodb").Document} Document
+ */
