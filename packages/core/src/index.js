64d834922bb4a6a6ebd1fdb63b3c3bfce13f8182
Jacob Bare <jacob@parameter1.com>
2022-12-21 11:19:11 -0600

    Export entire mongodb package

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,8 +1,3 @@
-export { ClientSession, Collection, MongoClient } from 'mongodb';
+export * from 'mongodb';
 export { filterMongoURL } from './filter-url.js';
 export { iterateMongoCursor } from './iterate-cursor.js';
-
-/**
- * @typedef {import("mongodb").Document} Document
- * @typedef {import("mongodb").Filter} Filter
- */
